a4302f2da5c16108569e419c811b2e4c901c6d77
Nuttachot Promrit <nuttachot@hotmail.com>
2018-10-28 22:18:24 +0700

    testjenkins

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Hello world!</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+	<style>
+	body {
+		background-color: white;
+		text-align: center;
+		padding: 50px;
+		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+	}
+	#logo {
+		margin-bottom: 40px;
+	}
+	</style>
+</head>
+<body>
+    <h1><?php echo "Hello World"; ?></h1>
+</body>
+</html>
